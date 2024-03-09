cf12a763f9b059848a15027ef212a867e0430858
Mario Acosta <macosta2015@my.fit.edu>
2024-03-09 10:00:50 -0500

    commit

--- wewe_waitinglist/src/components/helmet.jsx
+++ wewe_waitinglist/src/components/helmet.jsx
@@ -1,20 +1,37 @@
-import Helmet from "react-helmet";
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import './ResponsiveGoogleForm.css'; // Import CSS file for styling
 
-export default function NewWidget() {
-    return (
-        <>
-            <div
-                id="getWaitlistContainer"
-                data-waitlist_id="71"
-            ></div>
-            <Helmet>
-                <link
-                rel="stylesheet"
-                type="text/css"
-                href="https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.css"
-                />
-                <script src="https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.js"></script>
-            </Helmet>
-        </>
-    );
-}
+const ResponsiveGoogleForm = () => {
+  return (
+    <>
+      <div className="googleFormContainer">
+        {/* Provided Google Form iframe */}
+        <iframe
+          title="Google Form 3"
+          src="https://docs.google.com/forms/d/e/1FAIpQLSfzAIBnHYBjjAWteUxKE3w8mImaRZdgcKc0TSbQfDnAFRsKQw/viewform?embedded=true"
+          width="640"
+          height="100%" // Adjust as needed or remove the height attribute
+          frameBorder="0"
+          marginHeight="0"
+          marginWidth="0"
+          scrolling="yes" // Enable scrolling
+        >
+          Loading…
+        </iframe>
+      </div>
+      <Helmet>
+        {/* Stylesheet link */}
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.css"
+        />
+        {/* Script */}
+        <script src="https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.js"></script>
+      </Helmet>
+    </>
+  );
+};
+
+export default ResponsiveGoogleForm;
--- wewe_waitinglist/src/index.js
+++ wewe_waitinglist/src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import NewWidget2 from './components/helmet2';
+// import NewWidget2 from './components/helmet2';
 import NewWidget from './components/helmet';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <NewWidget2 />
+    {/* <NewWidget2 /> */}
     <NewWidget />
-
   </React.StrictMode>,
   document.getElementById('root')
 );
